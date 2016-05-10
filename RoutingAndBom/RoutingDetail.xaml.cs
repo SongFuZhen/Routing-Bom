976ca9e421a082a336c5b63696cc7324b5ab93bb
SongFuZhen <evil_song@126.com>
2016-05-10 17:02:00 +0800

    add split

---
+++ RoutingAndBom/RoutingDetail.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using Microsoft.Win32;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace RoutingAndBom
+{
+    /// <summary>
+    /// RoutingDetail.xaml 的交互逻辑
+    /// </summary>
+    public partial class RoutingDetail : MetroWindow
+    {
+        int click_count = 0;
+        public RoutingDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPart_Click(object sender, RoutedEventArgs e)
+        {
+            Thickness tn = new Thickness(0, 5, 0, 0);
+
+            TextBox pntb = new TextBox();
+            pntb.MinWidth = 200;
+            pntb.MaxWidth = 300;
+            pntb.Margin = tn;
+            pntb.Text = click_count + "PN";
+            pntb.Name = "PN" + click_count;
+
+            TextBox sdtb = new TextBox();
+            sdtb.MinWidth = 200;
+            sdtb.MaxWidth = 300;
+            sdtb.Margin = tn;
+            sdtb.Text = click_count + "SD";
+            sdtb.Name = "SD" + click_count;
+
+
+            TextBox qtb = new TextBox();
+            qtb.MinWidth = 200;
+            qtb.MaxWidth = 300;
+            qtb.Margin = tn;
+            qtb.Text = click_count + "QT";
+            qtb.Name = "QT" + click_count;
+
+            PN.Children.Add(pntb);
+            Standard.Children.Add(sdtb);
+            Quantity.Children.Add(qtb);
+
+            click_count++;
+        }
+
+        private void Finish_Routing(object sender, RoutedEventArgs e)
+        {
+            for (var i = 0; i < click_count; i++)
+            {
+                RoutingDescription.Text += ForEachTextBox(PN, "PN", i) + ForEachTextBox(Standard, "SD", i) + ForEachTextBox(Quantity, "QT", i);
+            }
+        }
+
+        private string ForEachTextBox(StackPanel sp, string spname, int count)
+        {
+            string TextBoxValue = "";
+            foreach (var ui in sp.Children)
+            {
+                if (ui is TextBox)
+                {
+                    TextBox current = ui as TextBox;
+                    if (current.Text == null || current.Text == "")
+                    {
+                    }
+                    else
+                    {
+                        if (current.Name == spname + count)
+                        {
+                            TextBoxValue = current.Text;
+                        }
+                    }
+                }
+            }
+            return TextBoxValue;
+        }
+
+        private void UploadImage_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Process.Start("explorer.exe", System.Environment.CurrentDirectory);
+
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            string currentPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            dlg.InitialDirectory = currentPath; // initial Path
+            dlg.DefaultExt = ".jpg"; // Default file extension
+            dlg.Filter = "图片文件(*.jpg,*.gif,*.bmp,*.png)|*.jpg;*.gif;*.bmp;*.png"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                ImageUrl.Content = dlg.FileName;
+                BitmapImage bmi = new BitmapImage(new Uri(dlg.FileName));
+                ShowImage.Source = bmi;
+            }
+        }
+
+
+        private async void SaveAsTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await this.ShowInputAsync("模板名称", "请输入模板名称：");
+
+            //用户点击cancel
+            if (result == null)
+                return;
+
+            //await this.ShowMessageAsync("Good", "保存成功！模板名称为" + result);
+        }
+    }
+}
--- RoutingAndBom/RoutingInfo.xaml.cs
+++ RoutingAndBom/RoutingInfo.xaml.cs
@@ -21,51 +21,60 @@ namespace RoutingAndBom
     public partial class RoutingInfo : MetroWindow
     {
         public string EPBA { get; set; }
-
         public RoutingInfo()
         {
             InitializeComponent();
+
+            SetTemplateSplitData();
+
+            SetRoutingListData();
         }
 
-        private void RoutingInfoAddBtn(object sender, RoutedEventArgs e)
+        private void SetTemplateSplitData()
         {
-            StackPanel sp = new StackPanel();
-            sp.Orientation = Orientation.Horizontal;
-            Thickness tn = new Thickness(0,10,0,0);
-            sp.Margin = tn;
-
-            TextBlock part_nr_lb = new TextBlock();
-            part_nr_lb.Text = "零件号";
-            TextBox part_nr_tb = new TextBox();
-            part_nr_tb.MinWidth = 200;
-            part_nr_tb.Height = 20;
-            TextBlock part_amount_lb = new TextBlock();
-            part_amount_lb.Text = "该零件数量";
-            Thickness thickness = new Thickness(20,0,0,0);
-            part_amount_lb.Margin = thickness;
-            TextBox part_amount_tb = new TextBox();
-            part_amount_tb.MinWidth = 200;
-            part_amount_tb.Height = 20;
-
-            sp.Children.Add(part_nr_lb);
-            sp.Children.Add(part_nr_tb);
-            sp.Children.Add(part_amount_lb);
-            sp.Children.Add(part_amount_tb);
-
-            Part_StackPanel.Children.Add(sp);
-        }
-
-        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (EPBA != null)
+           var template_data = new List<template_date>()
             {
-                routing_info_nr.Text = EPBA;
-                Routing_Info_Title.IsEnabled = false;
-                RoutingInfoBtn.Visibility = Visibility.Hidden;
-                image.Visibility = Visibility.Hidden;
-                CreateBtnRange.Visibility = Visibility.Hidden;
-                PartGroup.MaxHeight = 400;
-                PartGroup.IsEnabled = false;
+                new template_date {id="0", content="" },
+                new template_date {id="1", content="Template 1:贴IMM防尘纸" },
+                new template_date {id="2", content="Template 2:装USB防尘盖" },
+                new template_date {id="3", content="Template 3:组装触摸屏" },
+                new template_date {id="4", content="Template 4:手工检测" }
+            };
+
+            this.TemplateSplit.ItemsSource = template_data;
+        }
+
+        private void SetRoutingListData()
+        {
+            var routing_data = new List<routing_date>()
+            {
+                new routing_date {Nr="1", ProductName="贴IMM防尘纸，如图1", WorkStationNr="ZWT01", Status="Released", Release="unrelease"},
+                new routing_date {Nr="2", ProductName="装USB防尘盖，如图2", WorkStationNr="ZWT01", Status="new", Release="Release"},
+                new routing_date {Nr="3", ProductName="组装触摸屏，如图3", WorkStationNr="ZWT01", Status="Released", Release="unrelease"},
+            };
+            this.routing_list_data_grid.ItemsSource = routing_data;
+        }
+
+        private void NewRouting_Click(object sender, RoutedEventArgs e)
+        {
+            RoutingDetail rd = new RoutingDetail();
+            rd.ShowDialog();
+        }
+
+        private void TemplateSplit_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                //如何获取下拉框的值
+                RoutingDetail rd = new RoutingDetail();
+                rd.SaveAsTemplate.Visibility = Visibility.Hidden;
+                rd.Finish.HorizontalAlignment = HorizontalAlignment.Right;
+                rd.Finish.Width = 200;
+                rd.ShowDialog();
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
     }
